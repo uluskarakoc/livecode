3c22caf93b27d1f6fed190fe0c10767ff8e2a052
Thomas Hofmann <thomas@savoy-truffle.de>
2022-11-09 11:03:13 +0100

    html as tree

---
+++ 2-uib/2022-11-09-linking-id-mailto_and_colors/00-html-tree/html-file-as-tree.mm
@@ -0,0 +1,111 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="html" FOLDED="false" ID="ID_93366376" CREATED="1667983676645" MODIFIED="1667984307159" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="2.0">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="head" POSITION="right" ID="ID_883713429" CREATED="1667983753092" MODIFIED="1667984307157" HGAP_QUANTITY="13.250000022351742 pt" VSHIFT_QUANTITY="-11.999999642372142 pt">
+<edge COLOR="#0000ff"/>
+<node TEXT="meta" ID="ID_209606757" CREATED="1667983737150" MODIFIED="1667983757748">
+<node TEXT="charset=utf-8" ID="ID_1664130365" CREATED="1667983784663" MODIFIED="1667983793383"/>
+</node>
+<node TEXT="meta" ID="ID_153331864" CREATED="1667983778753" MODIFIED="1667983780225">
+<node TEXT="http-equiv..." ID="ID_1963863345" CREATED="1667983798974" MODIFIED="1667983807567"/>
+</node>
+<node TEXT="meta" ID="ID_888811110" CREATED="1667983780735" MODIFIED="1667983781837">
+<node TEXT="name=viewport" ID="ID_1897724262" CREATED="1667983809362" MODIFIED="1667983814494"/>
+</node>
+<node TEXT="title" ID="ID_1821775123" CREATED="1667983773637" MODIFIED="1667983774880">
+<node TEXT="&quot;HTML als Baum&quot;" ID="ID_373242217" CREATED="1667983817123" MODIFIED="1667983845431"/>
+</node>
+</node>
+<node TEXT="body" POSITION="right" ID="ID_1106845760" CREATED="1667983762034" MODIFIED="1667984311238" HGAP_QUANTITY="12.500000044703482 pt" VSHIFT_QUANTITY="30.749999083578615 pt">
+<edge COLOR="#00ff00"/>
+<node TEXT="h1" ID="ID_688598078" CREATED="1667983899967" MODIFIED="1667984311237" HGAP_QUANTITY="13.250000022351742 pt" VSHIFT_QUANTITY="-14.249999575316918 pt">
+<node TEXT="&quot;HTML als Baum&quot;" ID="ID_745378075" CREATED="1667984263086" MODIFIED="1667984270671"/>
+</node>
+<node TEXT="ol" ID="ID_512689007" CREATED="1667984087631" MODIFIED="1667984315892">
+<node TEXT="li" ID="ID_538103939" CREATED="1667984248584" MODIFIED="1667984250234">
+<node TEXT="&quot;headlines&quot;" ID="ID_603687283" CREATED="1667984254137" MODIFIED="1667984259330"/>
+</node>
+<node TEXT="ul" ID="ID_1978520621" CREATED="1667983965233" MODIFIED="1667984315889" HGAP_QUANTITY="13.250000022351742 pt" VSHIFT_QUANTITY="14.249999575316918 pt">
+<node TEXT="li" ID="ID_1569431601" CREATED="1667984183478" MODIFIED="1667984184391">
+<node TEXT="&quot;h1&quot;" ID="ID_1059925330" CREATED="1667983908290" MODIFIED="1667984220950"/>
+</node>
+<node TEXT="li" ID="ID_1965140197" CREATED="1667984185280" MODIFIED="1667984186162">
+<node TEXT="&quot;h2&quot;" ID="ID_375423480" CREATED="1667983911639" MODIFIED="1667984216146"/>
+</node>
+<node TEXT="li" ID="ID_1865101503" CREATED="1667984186576" MODIFIED="1667984187488">
+<node TEXT="&quot;...&quot;" ID="ID_106195128" CREATED="1667984204989" MODIFIED="1667984210778"/>
+</node>
+<node TEXT="li" ID="ID_1252328673" CREATED="1667983973814" MODIFIED="1667983975520">
+<node TEXT="&quot;h6&quot;" ID="ID_1041355494" CREATED="1667983914206" MODIFIED="1667983985140"/>
+</node>
+<node TEXT="ul" ID="ID_1124631056" CREATED="1667985661584" MODIFIED="1667985663052">
+<node TEXT="li" ID="ID_526583393" CREATED="1667986183282" MODIFIED="1667986184362"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
